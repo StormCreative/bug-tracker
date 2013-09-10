8fc2325e0d06bbd84bbb620aac00138e7cdc734a
Richard <richard@stormcreative.co.uk>
2013-09-10 14:37:00 +0100

    login page

--- app/views/templates/home/index.php
+++ app/views/templates/home/index.php
@@ -5,5 +5,8 @@
 		<p class="email"><i class="icon-envelope"></i><input type="email" name="user[email]" class="js-email" value="" placeholder="Email" /></p>
 		<p class="password"><i class="icon-lock"></i><input type="password" name="user[password]" class="js-password" value="" placeholder="Password"/></p>
 		<input type="submit" name="submit" class="login-button" value="Login" />
+		<a href="#" class="forgot-password js-forgot-password">Forgotten Password</a>
+		<p class="email hide"><i class="icon-envelope"></i><input type="email" name="user[email]" class="js-email" value="" placeholder="Email" /></p>
+		<input type="submit" name="submit" class="submit-button hide" value="Submit" />
 	</form>
 </div>
