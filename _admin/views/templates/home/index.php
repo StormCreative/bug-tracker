0057d2a926e261578531c052685912b7f6d61b8b
Richard <richard@stormcreative.co.uk>
2013-09-11 17:43:50 +0100

    front end

--- _admin/views/templates/home/index.php
+++ _admin/views/templates/home/index.php
@@ -3,8 +3,8 @@
     <h1>Content Management</h1>
         <form class="js-login-form" method="POST" name="login-form" action="#" enctype="multipart/form-data">
             <p class="js-error error_message" style="display: none;"></p>
-                <p class="email"><i class="icon-envelope"></i><input type="text" class="js-username" /></p>
-                <p class="password"><i class="icon-lock"></i><input type="password" class="js-password"/></p>
+                <p class="email"><i class="icon-envelope"></i><input type="text" class="js-username" placeholder="Email"/></p>
+                <p class="password"><i class="icon-lock"></i><input type="password" class="js-password" placeholder="Password"/></p>
                 <input type="submit" value="Login" class="login-button"/>
                 <a href="#" class="forgot-password js-forgot-password">Forgotten Password</a>
                  <p class="email hide"><i class="icon-envelope"></i><input type="email" name="user[email]" class="js-email" value="" placeholder="Email" /></p>
