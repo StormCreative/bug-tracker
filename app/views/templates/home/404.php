29d9741a169e963235b24361432f4870c9057f7a
Richard <richard@stormcreative.co.uk>
2013-09-12 14:01:17 +0100

    404

---
+++ app/views/templates/home/404.php
@@ -0,0 +1,13 @@
+<div class="wrapper">
+	<?php include "assets/includes/header.php"; ?>
+	<section class="main_list">
+		<aside class="filter">
+		</aside>
+		<article class="listing">
+			<h2>404 Error</h2>
+			<i class="icon-bug"></i>
+			<h3>Oh no! The page doesn't exist</h3>
+			<a href="<?php echo DIRECTORY; ?>bugs/listing" class="bug_back">Back to the Bug listing page!</a>
+		</article>
+	</section>
+</div>
