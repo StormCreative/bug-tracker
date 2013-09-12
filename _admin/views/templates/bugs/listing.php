936d365852a5357b21529cbd4dd4369899ab4e25
Dave Jones <davidjones@stormcreative.co.uk>
2013-09-12 14:33:42 +0100

    Updating some list stuff and merging with rich

--- _admin/views/templates/bugs/listing.php
+++ _admin/views/templates/bugs/listing.php
@@ -34,6 +34,7 @@
                   <a href="<?php echo DIRECTORY; ?>admin/bugs/listing#pending"><li class="js-tabs pending-tab active-tab" data-action="pending">Pending</li></a>
                   <a href="<?php echo DIRECTORY; ?>admin/bugs/listing#fixed"><li class="js-tabs fixed-tab" data-action="fixed">Fixed</li></a>
                   <a href="<?php echo DIRECTORY; ?>admin/bugs/listing#closed"><li class="js-tabs closed-tab" data-action="closed">Closed</li></a>
+                  <a href="<?php echo DIRECTORY; ?>admin/bugs/listing#flagged"><li class="js-tabs closed-tab" data-action="flagged">Flagged</li></a>
               </ul>
               <div class="pending">
                 <form action="<?php echo DIRECTORY; ?>admin/bugs/listing" method="POST">
@@ -175,8 +176,53 @@
 
                             </tbody>
                           </table>
+                      </div>
+                   </form>
+             </div>
+             <div class="flagged hide">
+                <form action="#" method="POST">
+                <div class="js-error"></div>
+                          <table width="100%" border="0" cellspacing="0" cellpadding="0" class="table_listing js-table">
+                            <thead>
+                              <tr>
+                                <th></th>
+                                <th>Name</th>
+                                <th>Summary</th>
+                                <th>Assigned to</th>
+                                <th><a href="<?php echo DIRECTORY; ?>admin/bugs/edit/?client_id=<?php echo $client_info['id']; ?>" class="add-button"><i class="icon-plus-sign"></i> Add</a></th>
+                              </tr>
+                            </thead>
+                            <tbody class="js-sortable js-body">
 
-                          <input type="hidden" name="type" value="archive" />
+                               <?php if( !!Bugs_model::get_bugs( 'flagged', $client_info['id'] ) ) : ?>
+                                    <?php foreach( Bugs_model::get_bugs( 'flagged', $client_info['id'] ) as $bug ) : ?>
+                                        <tr>
+                                            <td><input type="checkbox" name="user_id[]" value="<?php echo $bug[ 'id' ]; ?>"></td>
+                                            <td><?php echo $bug[ '_title' ]; ?></td>
+                                            <td><?php echo $bug[ 'summary' ]; ?></td>
+                                            <td>
+                                              <?php if( !!$bug[ 'assigned' ] ) : ?>
+                                                  <ul class="assigned">
+                                                  <?php foreach( $bug[ 'assigned' ] as $assigned ) : ?>
+                                                    <li><?php echo $assigned[ 'title' ] ?></li>
+                                                  <?php endforeach; ?>
+                                                </ul>
+                                              <?php endif; ?>
+                                            </td>
+                                            <td>
+                                              <a href="<?php echo DIRECTORY; ?>admin/bugs/edit/<?php echo $bug[ 'id' ]; ?>/?client_id=<?php echo $client_info['id']; ?>" class="edit_icon icon-edit"></a>
+                                              <?php if( cms_admin() ) : ?>
+                                                <a href="<?php echo DIRECTORY; ?>admin/bugs/change/open/?id=<?php echo $bug[ 'id' ]; ?>&client_id=<?php echo $client_info['id']; ?>" class="remove_icon icon-folder-open"></a>
+                                              <?php endif; ?>
+                                            </td>
+                                        </tr>
+                                    <?php endforeach; ?>
+                               <?php else : ?>
+                                <tr><td colspan="5" class="no_results">No results matched your criteria</td></tr>
+                              <?php endif; ?> 
+
+                            </tbody>
+                          </table>
                       </div>
                    </form>
              </div>
