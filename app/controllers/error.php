84e1386a425893b9e6a515c78a658e383bdb8fad
Dave Jones <davidjones@stormcreative.co.uk>
2013-09-12 11:40:22 +0100

    Set up the 404 stuff

--- app/controllers/error.php
+++ app/controllers/error.php
@@ -1,9 +1,14 @@
 <?php
 
-class error extends application_controller
+class error extends C_controller
 {
     public function access ()
     {
         die ( 'You need to be logged in to access this page.' );
+    }
+
+    public function error_404()
+    {
+    	die( 'This will be where the 404 page will appear' );
     }
 }
