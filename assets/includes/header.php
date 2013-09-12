82ca7d7e4e0f8cbb6b3fd3dc4635dd82dafedaa1
Richard <richard@stormcreative.co.uk>
2013-09-12 15:22:21 +0100

    nav

--- assets/includes/header.php
+++ assets/includes/header.php
@@ -2,4 +2,5 @@
 	<a href="<?php echo DIRECTORY; ?>"><img src="<?php echo DIRECTORY; ?>assets/images/logo.png" class="logo"/></a>
 	<h1><?php echo $client_information[ 'title' ]; ?> - Bug Tracker</h1>
 	<a href="<?php echo DIRECTORY; ?>bugs/logout" class="signout"><i class="icon-signout"></i> Log out</a>
+	<a href="<?php echo DIRECTORY; ?>bugs/logout" class="signout"><i class="icon-cogs"></i> Account Settings</a>
 </header>
