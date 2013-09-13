7628e98c5f220e68ab0bf25fbb22176c6a67bbf3
Dave Jones <davidjones@stormcreative.co.uk>
2013-09-13 16:50:35 +0100

    Pulling from rich

--- app/controllers/account.php
+++ app/controllers/account.php
@@ -4,7 +4,15 @@ class Account extends C_controller
 {
 	public function change_password()
 	{
-		
+		if( !!$_POST[ 'user' ] ) {
+
+			if() {
+
+			}
+			else if( $_POST[ 'user' ][ 'password' ] != $_POST[ 'user' ][ 'confirm_password' ] ) {
+				$this->addTag( 'error', 'Your password and confirm password need to be the same' );
+			}
+		}
 	}
 }
 
