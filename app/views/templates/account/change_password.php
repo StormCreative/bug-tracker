8d1d263238103f37a68362b06fa54dacda571c65
Richard <richard@stormcreative.co.uk>
2013-09-17 09:41:52 +0100

    account

--- app/views/templates/account/change_password.php
+++ app/views/templates/account/change_password.php
@@ -14,13 +14,13 @@
 	        		<input type="hidden" name="user[clients_contacts_id]" value="<?php echo $_SESSION[ 'client' ][ 'id' ]; ?>" />
 
 	        		<p><label for="password" class="form__info--title">Old password</label><input type="password" name="user[password]" class="js-validate" data-type="password" id="password" value="<?php echo $password; ?>" /></p>
-	        		<span class="js-error js-error-password error-msg error_message hide">Please provide your current password</span>
+	        		<p class="js-error js-error-password error-msg error_message hide">Please provide your current password</p>
 
 	        		<p><label for="new_password" class="form__info--title">New password</label><input type="password" name="user[new_password]" id="new_password" data-type="new-password" class="js-validate" value="<?php echo $new_password; ?>" /></p>
-	        		<span class="js-error js-error-new-password error-msg error_message hide">Please provide your new password</span>
+	        		<p class="js-error js-error-new-password error-msg error_message hide">Please provide your new password</p>
 
 	        		<p><label for="confirm_password" class="form__info--title">Confirm New Password</label><input type="password" name="user[confirm_password]" id="confirm_password" class="js-validate" data-type="confirm-new-password" value="<?php echo $confirm_password; ?>" /></p>
-	        		<span class="js-error js-error-confirm-new-password error-msg error_message hide">Please provide your confirm new password</span>
+	        		<p class="js-error js-error-confirm-new-password error-msg error_message hide">Please provide your confirm new password</p>
 
 	        		<p><label for="save" class="form__info--title save_btn_label">Save button</label><input type="submit" name="submit" value="Save" class="save-button" id="save"/></p>
 	        	</form>
